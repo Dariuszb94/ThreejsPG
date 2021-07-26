10d44713722b600c88c6b69a82e903c9776e51f7
Dariuszb94 <59793846+Dariuszb94@users.noreply.github.com>
2021-07-26 19:44:18 +0200

    Updates

---
+++ precache-manifest.c5b8fee52a17745a98247c1ac5e2964a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32ef45547c968ff5917ef6550c4bf5a9",
+    "url": "/ThreejsPG/index.html"
+  },
+  {
+    "revision": "6d899205775740a4211f",
+    "url": "/ThreejsPG/static/css/main.ff9cc5b0.chunk.css"
+  },
+  {
+    "revision": "efb417a1fef2dbed66f8",
+    "url": "/ThreejsPG/static/js/2.1e320f70.chunk.js"
+  },
+  {
+    "revision": "c9b31f27ce0803bcbda04bfd0eba249e",
+    "url": "/ThreejsPG/static/js/2.1e320f70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d899205775740a4211f",
+    "url": "/ThreejsPG/static/js/main.a9efed6b.chunk.js"
+  },
+  {
+    "revision": "9bd9bf7c860d5ff56217",
+    "url": "/ThreejsPG/static/js/runtime-main.6e5aa4fc.js"
+  }
+]);
